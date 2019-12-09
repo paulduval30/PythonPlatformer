ccf79efe55cf8678c05bd8d23b0e2f3e63b7d0a8
paulduval30 <paul.duval30@gmail.com>
2019-12-09 16:25:31 +0100

    On recommence tout

--- src/Game.py
+++ src/Game.py
@@ -1,6 +1,5 @@
-import pygame
-import random
-from Settings import *
+import random as random
+
 from Sprites import *
 
 class Game:
@@ -15,14 +14,13 @@ class Game:
     def new(self):
         self.all_sprites = pygame.sprite.Group()
         self.platforms = pygame.sprite.Group()
-        self.player = Personnage()
+        self.player = Personnage(self)
         self.all_sprites.add(self.player)
-        p1 = Plateforme(0, HEIGHT - 40, WIDTH, 40)
-        p2 = Plateforme(WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20)
-        self.all_sprites.add(p1)
-        self.all_sprites.add(p2)
-        self.platforms.add(p1)
-        self.platforms.add(p2)
+        for plat in PLAT_LIST:
+            p = Plateforme(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+            self.last_one = p
         self.run()
 
     def run(self):
@@ -35,10 +33,26 @@ class Game:
 
     def update(self):
         self.all_sprites.update()
-        hits = pygame.sprite.spritecollide(self.player, self.platforms, False)
-        if hits:
-            self.player.pos.y = hits[0].rect.top
-            self.player.vel.y = 0
+        if self.player.vel.y > 0:
+            hits = pygame.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
+
+        if self.player.rect.top <= HEIGHT / 4:
+            self.player.pos.y += abs(self.player.vel.y)
+            for plat in self.platforms:
+                plat.rect.y += abs(self.player.vel.y)
+                if plat.rect.top > HEIGHT:
+                    plat.kill()
+
+        while len(self.platforms) < 12:
+            width = random.randrange(50, 100)
+            p = Plateforme(random.randrange(0, WIDTH - width),self.last_one.rect.y - random.randrange(110, 140), width, 20)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
+            self.last_one = p
+
 
     def events(self):
         for event in pygame.event.get():
--- src/Sprites.py
+++ src/Sprites.py
@@ -5,10 +5,10 @@ vec = pygame.math.Vector2
 
 
 class Personnage(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
+        self.game = game
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(RED)
+        self.image = pygame.image.load("../res/perso.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.vx = 0
@@ -16,6 +16,13 @@ class Personnage(pygame.sprite.Sprite):
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+
+    def jump(self):
+        self.rect.x += 1
+        hits = pygame.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        if hits:
+             self.vel.y = -20
 
     def update(self):
         self.acc = vec(0, PLAYER_GRAV)
@@ -25,19 +32,27 @@ class Personnage(pygame.sprite.Sprite):
             self.acc.x = PLAYER_ACC
         if keys[pygame.K_LEFT]:
             self.acc.x = -PLAYER_ACC
-
+        if keys[pygame.K_SPACE]:
+            self.jump()
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
         self.acc.x -= self.vel.x * PLAYER_FRICTION
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
 
         self.rect.midbottom = self.pos
 
+        if self.pos.y > HEIGHT:
+            self.game.playing = False
+
 
 class Plateforme(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((width, height))
-        self.image.fill(GREEN)
+        self.image = pygame.image.load("../res/plateforme.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (width, height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
